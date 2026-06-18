033f21d19f60a052efa92abceae6f4e970745570
Tyler <87571592+Tila05@users.noreply.github.com>
2026-06-18 13:58:22 +0800

    feat: Add header section with a title, CTA's and a face card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import HeaderSection from "./components/HeaderSection";
 
 function App() {
   return (
     <>
       <header>
         <Navigation />
+        <HeaderSection />
       </header>
       <main>
-        <h1>Hello World!</h1>
+        <hr />
+        <section id="about">
+          <h2>About Section</h2>
+        </section>
+        <hr />
+        <section id="my-work">
+          <h2>My Work Section</h2>
+        </section>
+        <hr />
+        <section id="services">
+          <h2>Services Section</h2>
+        </section>
+        <hr />
+        <section id="contact">
+          <h2>Contact Section</h2>
+        </section>
       </main>
       <footer></footer>
     </>
---
+++ src/components/HeaderSection.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const HeaderSection = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-12 col-lg-8 mb-4">
+            <h1 className="fs-5 m-0">Hi , I'm Tyler</h1>
+            <h2 className="fs-1 mb-3">Front and Back-end Web Developer</h2>
+            <p>
+              I am a young web developer who has skills in both front-end and
+              back-end. I am always looking to learn new things whether that be
+              a framework, language, skill or project.
+            </p>
+            <a href="#my-work" className="button-link me-2">
+              View My Work
+            </a>
+            <a href="#contact" className="button-link-2">
+              Contact Me
+            </a>
+          </div>
+          <div className="col-12 col-lg-4 mb-4">
+            <img
+              src="https://placehold.co/400x500"
+              width="400"
+              height="500"
+              alt="Face card"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeaderSection;
