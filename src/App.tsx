4a7127e75455b9dc767a6a16494f5efa014f0e89
Tyler <87571592+Tila05@users.noreply.github.com>
2026-06-18 12:48:25 +0800

    feat: Add navbar to site with links to the sections on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+      <footer></footer>
     </>
   );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,40 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Navigation() {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <img
+            src="https://placehold.co/30x30"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Site Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="#about" className="nav-link">
+              About
+            </Nav.Link>
+            <Nav.Link href="#my-work" className="nav-link">
+              My Work
+            </Nav.Link>
+            <Nav.Link href="#services" className="nav-link">
+              Services
+            </Nav.Link>
+            <Nav.Link href="#contact" className="nav-link">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
