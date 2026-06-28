937de346488a70d477efff69a707730132b3015f
Tyler <87571592+Tila05@users.noreply.github.com>
2026-06-28 19:14:01 +0800

    feat: Update social media links with their actual links

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -1,13 +1,16 @@
 const SocialMediaLinks = () => {
   return (
     <div className="mb-4">
-      <a href="#">
+      <a href="mailto:tylerdewitdev@gmail.com">
         <i className="bi bi-envelope fs-4 me-3"></i>
       </a>
-      <a href="#">
+      <a
+        href="https://www.linkedin.com/in/tyler-de-wit-998976390/"
+        target="_blank"
+      >
         <i className="bi bi-linkedin fs-4 me-3"></i>
       </a>
-      <a href="#">
+      <a href="https://github.com/Tyler-de-Wit" target="_blank">
         <i className="bi bi-github fs-4 me-3"></i>
       </a>
     </div>
