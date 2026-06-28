07c63daac6fea6158f00307f9d593c54ee72fd0e
Tyler <87571592+Tila05@users.noreply.github.com>
2026-06-28 13:01:20 +0800

    feat: Add a component for the social media links

---
+++ src/components/SocialMediaLinks.tsx
@@ -0,0 +1,17 @@
+const SocialMediaLinks = () => {
+  return (
+    <div>
+      <a href="#">
+        <i className="bi bi-envelope fs-4 me-3"></i>
+      </a>
+      <a href="#">
+        <i className="bi bi-linkedin fs-4 me-3"></i>
+      </a>
+      <a href="#">
+        <i className="bi bi-github fs-4 me-3"></i>
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaLinks;
