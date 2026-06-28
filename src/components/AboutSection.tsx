480e36948915eeecec1a8861bffc49eb844d4173
Tyler <87571592+Tila05@users.noreply.github.com>
2026-06-28 19:25:33 +0800

    feat: Update icons for skill lists to better reflect the specific skill

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -11,23 +11,24 @@ const AboutSection = () => {
                 {/* Frameworks / Libraries */}
                 <h2 className="fs-4">Frameworks / Libraries</h2>
                 <ul className="list-group mb-4">
-                  <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Next.js
+                  <li className="list-group-item list-group-item-secondary list-group-item-action fs-5">
+                    <i className="bi bi-filetype-js me-3 fs-5"></i>
+                    Next.js
                   </li>
-                  <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>React
+                  <li className="list-group-item list-group-item-action fs-5">
+                    <i className="bi bi-filetype-js me-3 fs-5"></i>React
                   </li>
-                  <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Laravel
+                  <li className="list-group-item list-group-item-secondary list-group-item-action fs-5">
+                    <i className="bi bi-filetype-php me-3 fs-5"></i>Laravel
                   </li>
-                  <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>WordPress
+                  <li className="list-group-item list-group-item-action fs-5">
+                    <i className="bi bi-filetype-php me-3 fs-5"></i>WordPress
                   </li>
-                  <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Bootstsrap
+                  <li className="list-group-item list-group-item-secondary list-group-item-action fs-5">
+                    <i className="bi bi-filetype-css me-3 fs-5"></i>Bootstsrap
                   </li>
-                  <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>SASS
+                  <li className="list-group-item list-group-item-action fs-5">
+                    <i className="bi bi-filetype-css me-3 fs-5"></i>SASS
                   </li>
                 </ul>
               </div>
@@ -35,23 +36,23 @@ const AboutSection = () => {
                 {/* Languages */}
                 <h2 className="fs-4">Languages</h2>
                 <ul className="list-group mb-4">
-                  <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>JavaScript
+                  <li className="list-group-item list-group-item-secondary list-group-item-action fs-5">
+                    <i className="bi bi-javascript me-3 fs-5"></i>JavaScript
                   </li>
-                  <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>TypeScript
+                  <li className="list-group-item list-group-item-action fs-5">
+                    <i className="bi bi-typescript me-3 fs-5"></i>TypeScript
                   </li>
-                  <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>PHP
+                  <li className="list-group-item list-group-item-secondary list-group-item-action fs-5">
+                    <i className="bi bi-filetype-php me-3 fs-5"></i>PHP
                   </li>
-                  <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>CSS
+                  <li className="list-group-item list-group-item-action fs-5">
+                    <i className="bi bi-css me-3 fs-5"></i>CSS
                   </li>
-                  <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>HTML
+                  <li className="list-group-item list-group-item-secondary list-group-item-action fs-5">
+                    <i className="bi bi-filetype-html me-3 fs-5"></i>HTML
                   </li>
-                  <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>XML
+                  <li className="list-group-item list-group-item-action fs-5">
+                    <i className="bi bi-filetype-xml me-3 fs-5"></i>XML
                   </li>
                 </ul>
               </div>
@@ -63,19 +64,19 @@ const AboutSection = () => {
                 <h2 className="fs-4">Tools</h2>
                 <ul className="list-group mb-4">
                   <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Git
+                    <i className="bi bi-git me-3"></i>Git
                   </li>
                   <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Figma
+                    <i className="bi bi-easel me-3"></i>Figma
                   </li>
                   <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Photoshop
+                    <i className="bi bi-camera me-3"></i>Photoshop
                   </li>
                   <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Postman
+                    <i className="bi bi-filetype-json me-3"></i>Postman
                   </li>
                   <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Vercel
+                    <i className="bi bi-filetype-js me-3"></i>Vercel
                   </li>
                 </ul>
               </div>
@@ -84,23 +85,20 @@ const AboutSection = () => {
                 <h2 className="fs-4">Soft Skills</h2>
                 <ul className="list-group mb-4">
                   <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Efficient Time
+                    <i className="bi bi-alarm me-3"></i>Efficient Time
                     Management
                   </li>
                   <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Strong Leadership
+                    <i className="bi bi-person me-3"></i>Strong Leadership
                   </li>
                   <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Collaborative
-                    Teamwork
+                    <i className="bi bi-people me-3"></i>Collaborative Teamwork
                   </li>
                   <li className="list-group-item list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Effective
-                    Organisation
+                    <i className="bi bi-gear me-3"></i>Effective Organisation
                   </li>
                   <li className="list-group-item list-group-item-secondary list-group-item-action">
-                    <i className="bi bi-person-gear me-3"></i>Consistent
-                    Communication
+                    <i className="bi bi-chat me-3"></i>Consistent Communication
                   </li>
                 </ul>
               </div>
