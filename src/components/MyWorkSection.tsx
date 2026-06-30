bf52d3ede57270d36722fc216f24b433be31dce7
Tyler <87571592+Tila05@users.noreply.github.com>
2026-06-30 20:01:37 +0800

    feat: Turn the project cards into a carousel which automatically cycles through each card until the previous or next button is pressed

--- src/components/MyWorkSection.tsx
+++ src/components/MyWorkSection.tsx
@@ -1,13 +1,52 @@
 import projectCards from "../data/projectCards";
+import { useState, useEffect } from "react";
 
 const MyWorkSection = () => {
+  const [startIndex, setStartIndex] = useState(0);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  // Calculate the current visible project cards
+  const visibleProjectCards = Array.from({ length: 3 }, (_, i) => {
+    return projectCards[(startIndex + i) % projectCards.length];
+  });
+
+  // Handle previous card button press
+  const handlePreviousProjectCard = () => {
+    setStartIndex(
+      (prev) => (prev - 1 + projectCards.length) % projectCards.length,
+    );
+    setButtonClicked(true);
+  };
+
+  // Handle next card button press
+  const handleNextProjectCard = () => {
+    cycleToNextProjectCard();
+    setButtonClicked(true);
+  };
+
+  // Move the visible project cards along once
+  const cycleToNextProjectCard = () => {
+    setStartIndex((prev) => (prev + 1) % projectCards.length);
+  };
+
+  // Cycle through projects every 6 seconds
+  useEffect(() => {
+    if (!buttonClicked) {
+      const interval = setInterval(() => {
+        cycleToNextProjectCard();
+      }, 6000);
+
+      return () => clearInterval(interval);
+    }
+  }, [buttonClicked]);
+
   return (
     <>
       <h2 className="text-center mb-4 pt-4 heading-text">My Work</h2>
       <div className="container pb-5">
-        <div className="row g-5">
-          {projectCards.map((project) => (
-            <div className="col-12 col-md-6 col-lg-4" key={project.id}>
+        <div className="row g-4">
+          {visibleProjectCards.map((project) => (
+            <div className="col-12 col-md-6 col-lg-4 fade-in" key={project.id}>
               <div className="card h-100 card-background thin-border">
                 <a href={project.linkHref}>
                   <img
@@ -50,6 +89,20 @@ const MyWorkSection = () => {
               </div>
             </div>
           ))}
+          <div className="text-center">
+            <button
+              onClick={handlePreviousProjectCard}
+              className="btn button-link-2 me-2"
+            >
+              <i className="bi bi-arrow-left me-2"></i>Previous
+            </button>
+            <button
+              onClick={handleNextProjectCard}
+              className="btn button-link-2 ms-2"
+            >
+              Next<i className="bi bi-arrow-right ms-2"></i>
+            </button>
+          </div>
         </div>
       </div>
     </>
