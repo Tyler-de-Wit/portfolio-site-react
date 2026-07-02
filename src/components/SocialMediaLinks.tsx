440269827a11f00b560f064992c04e040cce1880
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-02 16:07:35 +0800

    feat: Update social media links to include link to resume

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -1,15 +1,22 @@
 const SocialMediaLinks = () => {
   return (
     <div className="mb-4">
+      {/* Email */}
       <a href="mailto:tylerdewitdev@gmail.com">
         <i className="bi bi-envelope fs-4 me-3"></i>
       </a>
+      {/* Resume */}
+      <a href="" target="_blank">
+        <i className="bi bi-file-person fs-4 me-3"></i>
+      </a>
+      {/* Linkedin */}
       <a
         href="https://www.linkedin.com/in/tyler-de-wit-998976390/"
         target="_blank"
       >
         <i className="bi bi-linkedin fs-4 me-3"></i>
       </a>
+      {/* Github */}
       <a href="https://github.com/Tyler-de-Wit" target="_blank">
         <i className="bi bi-github fs-4 me-3"></i>
       </a>
