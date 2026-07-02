d6f28899d10dfc80695f660033687e360d741faa
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-02 17:50:48 +0800

    feat: Add alt text to the project card images

--- src/data/projectCards.ts
+++ src/data/projectCards.ts
@@ -21,7 +21,7 @@ const projectCards: Project[] = [
   {
     id: 1,
     imageSrc: "/project-management-application.png",
-    imageAlt: "...",
+    imageAlt: "Project management application thumbnail",
     title: "Project Management Application",
     text: "App build with next.js that communicates with an API to create, read, update and delete projects, milestones, tasks, checklist items, comments and users.",
     liveSitelinkHref: "https://at3-web-app-using-frameworks-tyler.vercel.app/",
@@ -50,7 +50,7 @@ const projectCards: Project[] = [
   {
     id: 2,
     imageSrc: "/library-website.png",
-    imageAlt: "...",
+    imageAlt: "Library website thumbnail",
     title: "Library Website",
     text: "Built specifically for a client at the NMT Library, this website helps new students at NMTAFE complete common tasks in their first few weeks.",
     liveSitelinkHref: "https://mytafespace.screencraft.net.au/",
@@ -78,7 +78,7 @@ const projectCards: Project[] = [
   {
     id: 3,
     imageSrc: "/tilspics.png",
-    imageAlt: "...",
+    imageAlt: "TilsPics thumbnail",
     title: "TilsPics",
     text: "This is a site that I made for myself which allows me to upload, download and showcase my photography in it's full original quality.",
     liveSitelinkHref: "https://tilspics.com",
@@ -106,7 +106,7 @@ const projectCards: Project[] = [
   {
     id: 4,
     imageSrc: "/portfolio-website.png",
-    imageAlt: "...",
+    imageAlt: "Portfolio website thumbnail",
     title: "Portfolio Website",
     text: "This is the website that you are currently on, which I made for myself to showcase my skillset, passion and gloals.",
     liveSitelinkHref: "https://tylerdewit.com",
@@ -134,7 +134,7 @@ const projectCards: Project[] = [
   {
     id: 5,
     imageSrc: "/guitarmatchup.png",
-    imageAlt: "...",
+    imageAlt: "GuitarMatchup thumbnail",
     title: "GuitarMatchup",
     text: "This site showcases one of my other passions and it has a backend where I can edit the weekly guitars showcase from inside the site.",
     liveSitelinkHref: "https://guitarmatchup.com",
