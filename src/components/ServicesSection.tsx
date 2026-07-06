9d1528e18d797171db7cbc622d9a9d1aeaf3fdd8
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-06 11:57:00 +0800

    style: Fix formatting in code

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -4,12 +4,7 @@ import ServiceCard from "./ServiceCard";
 const ServicesSection = () => {
   return (
     <>
-      <h2
-        className="text-shadow-small
-      "
-      >
-        Services
-      </h2>
+      <h2 className="text-shadow-small">Services</h2>
 
       {serviceCards.map((service) => (
         <ServiceCard key={service.id} service={service} />
